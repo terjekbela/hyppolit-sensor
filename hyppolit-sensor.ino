74954cbde6f66a2e656c102acc3a70ef892b1f9a
Bela Terjek <bela@netc.hu>
2022-09-28 16:57:53 +0200

    Convert sketch to work with rp2040 picow

--- hyppolit-sensor.ino
+++ hyppolit-sensor.ino
@@ -12,27 +12,20 @@
 #include "config-secrets.h"
 #include "circuit.h"
 
-#include <ArduinoLowPower.h>
-#include <SPI.h>
-#include <WiFiNINA.h>
-#include <ECCX08.h>
+#include <Wire.h>
+#include <WiFi.h>
 
-#include <Adafruit_Sensor.h>
 #include <Adafruit_MCP9808.h>
 #include <Adafruit_BME280.h>
+
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Runtime variables
 ////////////////////////////////////////////////////////////////////////////////
 
+WiFiMulti wifiMulti;
 WiFiClient wifiClient;
-#if defined(NET_CLIENT_IP)
-IPAddress ipIP (NET_CLIENT_IP);
-IPAddress ipDNS(NET_CLIENT_DNS);
-IPAddress ipGW (NET_CLIENT_GW);
-IPAddress ipNET(NET_CLIENT_NET);
-#endif
 
 
 
@@ -48,88 +41,54 @@ void setup() {
   pinMode(SENSOR_POWER, OUTPUT);
   digitalWrite(SENSOR_POWER, LOW);
 
-  WiFiDrv::pinMode(LED_RED,   OUTPUT); //red
-  WiFiDrv::pinMode(LED_GREEN, OUTPUT); //green
-  WiFiDrv::pinMode(LED_BLUE,  OUTPUT); //blue
-  led(2,4,8,5000);
-
-  WiFi.end();
-  ECCX08.begin();
-  ECCX08.end();
+  wifiMulti.addAP(NET_CLIENT_SSID1, NET_CLIENT_PASS1);
+  if (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("Unable to connect, rebooting...");
+    delay(5000);
+    rp2040.reboot();
+  }
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 // main loop
 void loop() {
-  int   wifiStatus       = WL_IDLE_STATUS;
-  pinMode(SENSOR_POWER, OUTPUT);
-  digitalWrite(SENSOR_POWER, HIGH);
-  delay(100);
+  const char*    host = NET_SERVER_NAME;
+  const uint16_t port = NET_SERVER_PORT;
   
-  float batteryValue     = sensorValueBattery();
-  float temperatureValue = sensorValueMCP9808();
-  if(isnan(temperatureValue)) {
-    temperatureValue = sensorValueBME280Temperature();
-  }
+  float batteryValue     = 50;
+  float temperatureValue = sensorValueMCP9808();                       // getting sensor values
   float humidityValue    = sensorValueBME280Humidity();
   float pressureValue    = sensorValueBME280Pressure();
-  digitalWrite(SENSOR_POWER, LOW);
-  pinMode(SENSOR_POWER, INPUT);
-  led(0,0,8);
-  Serial.print("Connecting: ");
-  Serial.println(millis());
-#if defined(NET_CLIENT_IP)
-  WiFi.config(ipIP, ipDNS, ipGW, ipNET);
-#endif
-  wifiStatus = WiFi.begin(NET_CLIENT_SSID, NET_CLIENT_PASS);
-  Serial.print("Connected: ");
-  Serial.println(millis());
-  if ( wifiStatus == WL_CONNECTED) {
-    if (wifiClient.connect(IPAddress(NET_SERVER_IP), NET_SERVER_PORT)) {
-      led(0,4,0);
-      wifiClient.print("GET /sensor/1?battery=");
-      wifiClient.print(batteryValue);
-      if(!isnan(temperatureValue)) {
-        wifiClient.print("&temperature=");
-        wifiClient.print(temperatureValue);
-      }
-      if(!isnan(humidityValue)) {
-        wifiClient.print("&humidity=");
-        wifiClient.print(humidityValue);
-      }
-      if(!isnan(pressureValue)) {
-        wifiClient.print("&pressure=");
-        wifiClient.print(pressureValue);
-      }
-      wifiClient.println(" HTTP/1.0");
-      wifiClient.println();
-      delay(200);
-      led(0,0,0);
-    } else {
-      led(2,0,0,200);
+
+  Serial.println("Server connecting");
+  if (wifiClient.connect(host, port)) {
+    Serial.println("Server connected");
+    wifiClient.print("GET /sensor/1?battery=");
+    wifiClient.print(batteryValue);
+    if(!isnan(temperatureValue)) {
+      wifiClient.print("&temperature=");
+      wifiClient.print(temperatureValue);
     }
+    if(!isnan(humidityValue)) {
+      wifiClient.print("&humidity=");
+      wifiClient.print(humidityValue);
+    }
+    if(!isnan(pressureValue)) {
+      wifiClient.print("&pressure=");
+      wifiClient.print(pressureValue);
+    }
+    wifiClient.println(" HTTP/1.1");
+    wifiClient.print("host: ");
+    wifiClient.println(host);
+    wifiClient.println();
+    Serial.println("Request sent");
   } else {
-    led(2,0,0,200);
+    Serial.println("Server failed to connect");
   }
-  WiFi.end();
+  Serial.println();
 
-  digitalWrite(SENSOR_SCL, HIGH);
-  digitalWrite(SENSOR_SDA, HIGH);
-  LowPower.sleep(56000);
-  //delay(60000);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// LED library
-////////////////////////////////////////////////////////////////////////////////
-
-void led(byte red, byte green, byte blue) {
-  WiFiDrv::analogWrite(LED_RED,   red);
-  WiFiDrv::analogWrite(LED_GREEN, green);
-  WiFiDrv::analogWrite(LED_BLUE,  blue);
-}
-
-void led(byte red, byte green, byte blue, int wait) {
-  led(red, green, blue);
-  delay(wait);
-  led(0,0,0);
+  Serial.println("Delaying 10s...");
+  delay(10000);
 }
